80413b017dd238053c19be2f73aa1daf16fea29a
zihuazhuanche <111187777+zihuazhuanche@users.noreply.github.com>
2022-12-29 22:49:19 +0800

    Create iii.js

---
+++ iii.js
@@ -0,0 +1,14 @@
+/*
+
+instapaper解锁Pro--Eric
+
+https://www.instapaper.com/api/subscription_status
+
+*/
+
+
+
+
+body = $response.body.replace(/.+/,"ipsubnow=1585578158&ipsubexp=4079167311&ipsub=1")
+
+$done({body});
